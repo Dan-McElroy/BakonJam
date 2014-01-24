493b40feef08fca91f60d44be2e30e40e3e90332
Dan McElroy <daniel@mcelroys.net>
2014-01-24 22:39:28 +0000

    Environment scripts started.

---
+++ BAKON/BAKON/Assets/_Scripts/env_Bakon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class env_Bakon : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		level = GameObject.FindGameObjectWithTag ("Level");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/*void OnCollisionEnter2D (Collision2D coll)
+	{
+		if ((ent_Player player = coll.gameObject.GetComponent<ent_Player> ()) != null) 
+		{
+			player.
+		}
+	}*/
+
+}
---
+++ BAKON/BAKON/Assets/_Scripts/env_Level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Embarked upon by Daniel Emeritus McElroy at 22:24, Friday 24th January.
+public class env_Level : MonoBehaviour {
+
+	// 0 = neutral, 1 = playerOne, 2 = playerTwo
+	public int currentReality;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
