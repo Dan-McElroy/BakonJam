fc2bb9ca63bbddae44d285122f5d1927ca774cf2
garfunkle <j.deaves18@googlemail.com>
2014-01-25 18:20:53 +0000

    Drop offs
    Added proper collision for all drop off points

--- BAKON/BAKON/Assets/_Scripts/Environment/env_Dropoff.cs
+++ BAKON/BAKON/Assets/_Scripts/Environment/env_Dropoff.cs
@@ -12,34 +12,31 @@ public class env_Dropoff : MonoBehaviour {
 	void Update () {
 	
 	}
-	
-	
-	
-	void OnCollisionEnter2D (Collision2D coll)
+
+	void OnTriggerEnter2D(Collider2D coll)
 	{
-		
 		if (coll.gameObject.tag=="Player") 
 		{
-
+			
 			ent_Player obj = coll.gameObject.GetComponent<ent_Player>();
 			if(obj.HasBacon)
 			{
-
+				
 				// Increase player score
 				ent_Statistics stats = coll.gameObject.GetComponent<ent_Statistics>();
 				stats.AddScore();
-
+				
 				obj.HasBacon = false;
-
-
+				
+				
 				// Reset bakonz
 				foreach(var bacon in GameObject.FindGameObjectsWithTag ("Bakon"))
 				{
 					bacon.transform.position = GameObject.FindWithTag("bacon_pedestal").transform.position;
 					bacon.transform.parent = null;
 				}
-
-
+				
+				
 			} // CLOSE if(obj.HasBacon)
 		}
 	}
