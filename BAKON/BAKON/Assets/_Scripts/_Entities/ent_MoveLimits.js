f787753864f71ea9d8bb59ed7ad3437268128e67
58gamez <przemek@58gamez.com>
2014-01-25 02:20:47 +0000

    Scene Changes

---
+++ BAKON/BAKON/Assets/_Scripts/_Entities/ent_MoveLimits.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+var max_x : float = 18;
+var min_x : float = -18;
+var max_y : float = 18;
+var min_y : float = -18;
+
+
+function Update () {
+	if( transform.position.x>max_x ){
+		transform.position.x=max_x;
+	}
+	
+	if( transform.position.x<min_x ){
+		transform.position.x=min_x;
+	}
+	
+	if( transform.position.y>max_y ){
+		transform.position.y=max_y;
+	}
+	
+	if( transform.position.y<min_y ){
+		transform.position.y=min_y;
+	}
+
+}
