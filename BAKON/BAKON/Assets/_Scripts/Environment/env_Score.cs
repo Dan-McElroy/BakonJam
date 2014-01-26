1b855c84468107c5e21675481d6d508b9df181b8
Dan McElroy <daniel@mcelroys.net>
2014-01-26 03:04:23 +0000

    Started end game script.

--- BAKON/BAKON/Assets/_Scripts/Environment/env_Score.cs
+++ BAKON/BAKON/Assets/_Scripts/Environment/env_Score.cs
@@ -3,17 +3,63 @@ using System.Collections;
 
 public class env_Score : MonoBehaviour {
 
-	public int[] scores;
+	public float timeGameStarted;
+	public int winningScore = 10;
+	public ent_Statistics[] players;
+	public int winnerIndex;
+	public float finalDuration = 1f;
 
-	public ent_Player[] players;
+	public GameObject startMenu;
+	public GameObject playerObjectContainer;
+
+	private bool begun = false;
+
 
 	// Use this for initialization
-	void Start () {
+	void Begin () {
 
+		GameObject[] playerObjects = GameObject.FindGameObjectsWithTag ("Player");
+		for (int i = 0; i < playerObjects.Length; i++)
+		{
+			players[i] = playerObjects[i].GetComponent<ent_Statistics>();
+		}
+		timeGameStarted = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if (!begun && !startMenu.activeSelf)
+		{
+			Begin ();
+			begun = true;
+		}
+
+		for (int i = 0; i < players.Length; i++)
+		{
+			if (players[i].Score == winningScore)
+			{
+				winnerIndex = i;
+				EndGame ();
+			}
+		}
+	}
+
+	void EndGame()
+	{
+		finalDuration = Time.time - timeGameStarted;
+		playerObjectContainer.SetActive (false);
+	}
+
+	public string winnerMessage()
+	{
+		if (winnerIndex == null)
+		{
+			return "Nobody won!";
+		}
+		else 
+		{
+			return "Congratulations, player " + winnerIndex + "!";
+		}
 	}
 }
