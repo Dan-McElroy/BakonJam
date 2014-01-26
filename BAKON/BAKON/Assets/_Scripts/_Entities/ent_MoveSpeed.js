34a236313e1228823809c653754e9116fcb29529
58gamez <przemek@58gamez.com>
2014-01-26 04:21:21 +0000

    iadsdasdas

---
+++ BAKON/BAKON/Assets/_Scripts/_Entities/ent_MoveSpeed.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+var prevPos : Vector3;
+var animator: Animator;
+
+function Start () {
+	
+
+}
+
+function Update () {
+	var movSpeed : float = 0;
+	movSpeed = Vector3.Distance(prevPos,transform.position);
+	
+	animator.SetFloat("MoveSpeed", movSpeed);
+	
+	//FLIP CHARACTER
+	if(movSpeed>0.01){
+		if(prevPos.x>transform.position.x ){
+			//MOVING LEFT
+			transform.localScale.x =Mathf.Abs(transform.localScale.x);
+		}else{
+			//MOVING RIGHT
+			transform.localScale.x =Mathf.Abs(transform.localScale.x)*-1;
+		}
+	}
+	
+	prevPos=transform.position;
+	
+}
---
+++ BAKON/BAKON/Assets/_Scripts/_Gui/gui_playerScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gui_playerScore : MonoBehaviour {
+
+	// Use this for initialization
+
+	public ent_Statistics stats;
+	 
+	
+	// Update is called once per frame
+	void Update () {
+		string score ="";
+		if(stats.Score<10){
+			score = "0";
+		}
+		
+		GetComponent<TextMesh>().text = score + stats.Score;
+		
+	}
+}
