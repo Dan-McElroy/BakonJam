471314d9a5944dca12b6cc3bca41a42c0eb63885
58gamez <przemek@58gamez.com>
2014-01-26 10:45:20 +0000

    dadsd

---
+++ BAKON/BAKON/Assets/_Scripts/_Entities/ent_ShowInvincible.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+var nextCheck : float = 0;
+var s_render : SpriteRenderer;
+
+
+function Start () {
+	s_render = GetComponent(SpriteRenderer);
+}
+
+
+function Update () {
+	if(Time.time> nextCheck){
+		if(!GetComponent(BoxCollider2D).enabled){
+			s_render.enabled =!s_render.enabled;
+		}else{
+			s_render.enabled =true;
+		}
+		
+		nextCheck = Time.time+0.05; 
+		
+		//SPRITE ON AND OFF
+		
+	}
+}
---
+++ BAKON/BAKON/Assets/_Scripts/_Environment/env_StartGame.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var hideGO : GameObject[];
+var showGO : GameObject[];
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown ("space")){
+			print ("space key was pressed");
+			
+			for(var obj2:GameObject in showGO){
+					obj2.SetActive(true);
+			}	
+			
+			for(var obj:GameObject in hideGO){
+					obj.SetActive(false);
+			}
+	}	
+}
